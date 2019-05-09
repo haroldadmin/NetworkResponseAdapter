47360626546264cf4cefbf200841c941636a55f3
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-05-09 22:51:26 +0530

    feat: Add NetworkResponse sealed class

---
+++ src/main/kotlin/com/haroldadmin/cnradapter/NetworkResponse.kt
@@ -0,0 +1,20 @@
+package com.haroldadmin.cnradapter
+
+import java.io.IOException
+
+sealed class NetworkResponse<out T : Any, out U : Any> {
+    /**
+     * A request that resulted in a response with a 2xx status code that has a body.
+     */
+    data class Success<T : Any>(val body: T) : NetworkResponse<T, Nothing>()
+
+    /**
+     * A request that resulted in a response with a non-2xx status code.
+     */
+    data class ServerError<U : Any>(val body: U?, val code: Int) : NetworkResponse<Nothing, U>()
+
+    /**
+     * A request that didn't result in a response.
+     */
+    data class NetworkError(val error: IOException) : NetworkResponse<Nothing, Nothing>()
+}
