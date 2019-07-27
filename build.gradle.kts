9a39e03bb4f6199f57831ae0cc87b3dd99926a34
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-07-27 20:52:29 +0530

    feat: Update Kotlin to 1.3.41 and bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm").version("1.3.31")
+    id("org.jetbrains.kotlin.jvm").version("1.3.41")
     maven
 }
 
@@ -15,11 +15,11 @@ val test by tasks.getting(Test::class) {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
-    implementation("com.squareup.retrofit2:retrofit:2.5.0")
-    implementation("com.squareup.okhttp3:okhttp:3.14.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC")
+    implementation("com.squareup.retrofit2:retrofit:2.6.0")
+    implementation("com.squareup.okhttp3:okhttp:4.0.1")
 
-    testImplementation("com.squareup.okhttp3:mockwebserver:3.14.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.0.1")
     testImplementation("com.google.guava:guava:26.0-jre")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
 }
