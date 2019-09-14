3ec41eafdc6f203d5a9b94298f57ea0402332d67
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-09-14 15:35:50 +0530

    feat: Add test for suspending functions with executeWithRetry

--- src/test/kotlin/com/haroldadmin/cnradapter/RetryTest.kt
+++ src/test/kotlin/com/haroldadmin/cnradapter/RetryTest.kt
@@ -4,6 +4,7 @@ import io.kotlintest.Spec
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.DescribeSpec
+import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.SocketPolicy
@@ -23,11 +24,11 @@ class ExtensionsTest : DescribeSpec() {
         server = MockWebServer()
         executor = Executors.newSingleThreadExecutor()
         retrofit = Retrofit.Builder()
-            .baseUrl(server.url("/"))
-            .callbackExecutor(executor)
-            .addCallAdapterFactory(NetworkResponseAdapterFactory())
-            .addConverterFactory(StringConverterFactory())
-            .build()
+                .baseUrl(server.url("/"))
+                .callbackExecutor(executor)
+                .addCallAdapterFactory(NetworkResponseAdapterFactory())
+                .addConverterFactory(StringConverterFactory())
+                .build()
         service = retrofit.create(Service::class.java)
     }
 
@@ -40,22 +41,45 @@ class ExtensionsTest : DescribeSpec() {
     init {
         describe("Execute with retry") {
 
-            repeat(9) {
-                val mockResponse = MockResponse()
-                server.enqueue(mockResponse.apply { mockResponse.socketPolicy = SocketPolicy.DISCONNECT_AFTER_REQUEST })
+            context("Deferred response") {
+                repeat(9) {
+                    val mockResponse = MockResponse()
+                    server.enqueue(mockResponse.apply { mockResponse.socketPolicy = SocketPolicy.DISCONNECT_AFTER_REQUEST })
+                }
+
+                server.enqueue(MockResponse().setBody("Hi!"))
+
+                val response = executeWithRetry(times = 10) {
+                    service.getText().await()
+                }
+
+                it("Should end up with NetworkResponse.Success after 10 retries") {
+                    (response is NetworkResponse.Success) shouldBe true
+                    with(response as NetworkResponse.Success) {
+                        body shouldBe "Hi!"
+                        headers shouldNotBe null
+                    }
+                }
             }
 
-            server.enqueue(MockResponse().setBody("Hi!"))
+            context("Suspending response") {
+                repeat(9) {
+                    val mockResponse = MockResponse()
+                    server.enqueue(mockResponse.apply { mockResponse.socketPolicy = SocketPolicy.DISCONNECT_AFTER_REQUEST })
+                }
 
-            val response = executeWithRetry(times = 10) {
-                service.getText().await()
-            }
+                server.enqueue(MockResponse().setBody("Hi!"))
 
-            it("Should end up with NetworkResponse.Success after 10 retries") {
-                (response is NetworkResponse.Success) shouldBe true
-                with(response as NetworkResponse.Success) {
-                    body shouldBe "Hi!"
-                    headers shouldNotBe null
+                val response = executeWithRetry(times = 10) {
+                    runBlocking { service.getTextSuspend() }
+                }
+
+                it("Should end up with NetworkResponse.Success after 10 retries") {
+                    (response is NetworkResponse.Success) shouldBe true
+                    with(response as NetworkResponse.Success) {
+                        body shouldBe "Hi!"
+                        headers shouldNotBe null
+                    }
                 }
             }
         }
