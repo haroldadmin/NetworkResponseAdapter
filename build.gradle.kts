adb4dd6f6c990802c014d8a3bc12aa3b9f4002c0
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2021-07-04 08:58:03 +0530

    feat: Bump version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ publishing {
         create<MavenPublication>("NetworkResponseAdapter") {
             groupId = "com.github.haroldadmin"
             artifactId = "NetworkResponseAdapter"
-            version = "4.2.1"
+            version = "4.2.2"
 
             from(components["java"])
         }
